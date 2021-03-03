d40dfc0fbb12aaf67aa321af1fa89403cce088d4
chihyuanyu <>
2021-03-03 19:34:34 +0800

    修改出版社

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -18,16 +20,37 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        Publisher manning_publisher = new Publisher();
+        manning_publisher.setName("Manning Publications Co.");
+        manning_publisher.setAddress("20 Baldwin Road, PO Box 761 Shelter Island, NY");
+
+        Publisher packt_publisher = new Publisher();
+        packt_publisher.setName("Packt Publishing");
+        packt_publisher.setAddress("Birmingham, UK");
+
+        Publisher ch_publisher = new Publisher();
+        ch_publisher.setName("深智數位");
+        ch_publisher.setAddress("台北市信義區基隆路");
+
+        publisherRepository.save(manning_publisher);
+        publisherRepository.save(packt_publisher);
+        publisherRepository.save(ch_publisher);
+
+        System.out.println("Publisher Count: " + publisherRepository.count());
+
         Author author1 = new Author("Craig", "Walls");
-        Book book1 = new Book("Spring Boot in Action", "1617292540", "MANNING");
+        Book book1 = new Book("Spring Boot in Action", "1617292540", manning_publisher);
         author1.getBooks().add(book1);
         book1.getAuthors().add(author1);
 
@@ -35,7 +58,7 @@ public class BootStrapData implements CommandLineRunner {
         bookRepository.save(book1);
 
         Author author2 = new Author("Greg L.", "Turnquist");
-        Book book2 = new Book("Learning Spring Boot 2.0", "1786463784", "Packt");
+        Book book2 = new Book("Learning Spring Boot 2.0", "1786463784", packt_publisher);
         author2.getBooks().add(book2);
         book2.getAuthors().add(author2);
 
@@ -43,7 +66,7 @@ public class BootStrapData implements CommandLineRunner {
         bookRepository.save(book2);
 
         Author author3 = new Author("中華", "龍");
-        Book book3 = new Book("極速開發 Java 大型系統:Spring Boot 又輕又快又好學", "9865501643", "深X數位");
+        Book book3 = new Book("極速開發 Java 大型系統:Spring Boot 又輕又快又好學", "9865501643", ch_publisher);
         author3.getBooks().add(book3);
         book3.getAuthors().add(author3);
 
--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -16,12 +16,14 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
-    private String publisher;
+
+    @OneToOne
+    private Publisher publisher;
 
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name = "author_id"))
-    private Set<Author> authors = new HashSet<>();
+    private Set<Author> authors = new HashSet<Author>();
 
     public Book() {
     }
@@ -31,16 +33,17 @@ public class Book {
         this.isbn = isbn;
     }
 
-    public Book(String title, String isbn, String publisher) {
+    public Book(String title, String isbn, Publisher publisher) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
     }
 
-    public Book(String title, String isbn, Set<Author> authors) {
+    public Book(String title, String isbn, Publisher publisher, Set<Author> authors) {
         this.title = title;
         this.isbn = isbn;
         this.authors = authors;
+        this.publisher = publisher;
     }
 
     public Long getId() {
@@ -75,11 +78,11 @@ public class Book {
         this.authors = authors;
     }
 
-    public String getPublisher() {
+    public Publisher getPublisher() {
         return publisher;
     }
 
-    public void setPublisher(String publisher) {
+    public void setPublisher(Publisher publisher) {
         this.publisher = publisher;
     }
 
